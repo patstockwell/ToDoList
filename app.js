ea1cbca196df30abd297b9b91b6e88b7546534f4
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-05-27 00:53:52 +1000

    create todo app

---
+++ app.js
@@ -0,0 +1,36 @@
+var myApp = (function(){
+	// the list itself
+	var list = [];
+
+	// DOM cache
+	var $wrapper = $('.wrapper');
+	var $input = $wrapper.find('input');
+	var $button = $wrapper.find('button');
+	var $allItems = $wrapper.find('ul');
+
+	// bind listener
+	$button.on('click', addItem);
+
+	// render list to html
+	render();
+
+	function render(){
+		for(var x = 0; x < list.length; x++){
+			$allItems.append('<li>' + list[x] + '</li>');
+		}
+	}
+
+	function addItem(){
+		item = $input.val();
+		list.push(name);
+		$allItems.append('<li>' + item + '</li>');
+		$input.val('');
+	}
+
+	function clearInput(){
+
+	}
+
+	return addItem;
+
+})()
