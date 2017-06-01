64a3d0463f98ecaa69ac3c6c10fee66a71beed28
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-06-01 16:10:37 +1000

    remove npm build dependencies and add mustache templating

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
 var myApp = (function(){
-	// the list itself
-	var list = [];
+	$(document).ready(function(){
 
-	// DOM cache
-	var $wrapper = $('.wrapper');
-	var $input = $wrapper.find('input');
-	var $button = $wrapper.find('button');
-	var $allItems = $wrapper.find('ul');
 
-	// bind listener
-	$button.on('click', addItem);
+		// the list itself
+		var list = [
+			{details: "first"},
+			{details: "second"}
+		];
 
-	// render list to html
-	render();
+		// DOM cache
+		var $wrapper = $('.wrapper');
+		var $input = $wrapper.find('input');
+		var $button = $wrapper.find('button');
+		var $allItems = $wrapper.find('ul');
+		var template = '<li>{{details}}</li>'
 
-	function render(){
-		for(var x = 0; x < list.length; x++){
-			$allItems.append('<li>' + list[x] + '</li>');
+		// bind listener
+		$button.on('click', addItem);
+
+		// render list to html
+		render();
+
+		function render(){
+			for(var x = 0; x < list.length; x++){
+				$allItems.append(Mustache.render(template, list[x]));
+			}
 		}
-	}
 
-	function addItem(){
-		item = $input.val();
-		list.push(name);
-		$allItems.append('<li>' + item + '</li>');
-		$input.val('');
-	}
+		function addItem(){
+			// get the input and create object
+			item = {
+				details: $input.val()
+			}
+			// put it in the array
+			list.push(item);
+			// render the new item to the page
+			$(Mustache.render(template, item))
+			.css({"display":"none"})
+			.appendTo($allItems)
+			.slideDown(300);
+
+			// clear the input
+			$input.val('');
+		}
 
 
-	return addItem;
+		return {
+			addItem: addItem,
+			list, list
+		};
+	});
 
-})()
+})();
